85f07b01ceeaa7faa959ccfdbb173182df129b2c
Chas3Pap3rs <futurefineants@gmail.com>
2024-11-06 13:38:39 -0500

    next config file changes for images not populating in github pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,25 @@
+// next.config.mjs
+
+// Check if we are running on GitHub Actions
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+let assetPrefix = '';
+let basePath = '';
+
+// Configure the asset prefix and base path for GitHub Pages
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, ''); // Get the repo name
+  assetPrefix = `/${repo}/`;
+  basePath = `/${repo}`;
+}
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',        // Enables static HTML export for GitHub Pages
+  images: {
+    unoptimized: true,     // Disables Next.js image optimization
+  },
+  assetPrefix: assetPrefix, // Set the asset prefix for GitHub Pages
+  basePath: basePath,       // Set the base path for GitHub Pages
+};
 
 export default nextConfig;
