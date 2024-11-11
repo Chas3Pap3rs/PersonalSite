a43c5daa06cf5e6a3da2ee2ad97af248fa72b6a4
Chas3Pap3rs <futurefineants@gmail.com>
2024-11-11 18:16:07 -0500

    More updates to json and yml files to fix 403 error on webpage via Hostinger

--- next.config.mjs
+++ next.config.mjs
@@ -1,25 +1,16 @@
-// next.config.mjs
-
-// Check if we are running on GitHub Actions
 const isGithubActions = process.env.GITHUB_ACTIONS || false;
-
-// Set base path and asset prefix based on whether we are on GitHub Actions
 const repoName = isGithubActions ? process.env.GITHUB_REPOSITORY.split('/')[1] : '';
-const basePath = isGithubActions ? `/${repoName}` : '';
-const assetPrefix = isGithubActions ? `/${repoName}/` : '';
 
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', // Enables static HTML export for GitHub Pages
+  output: 'export',
   images: {
-    unoptimized: true, // Disables Next.js image optimization for static export
+    unoptimized: true,
   },
-  assetPrefix: assetPrefix || undefined, // Only use assetPrefix when in production on GitHub Pages
-  basePath: basePath || undefined,       // Only use basePath when in production on GitHub Pages
+  assetPrefix: isGithubActions ? `/${repoName}/` : '',
+  basePath: isGithubActions ? `/${repoName}` : '',
   publicRuntimeConfig: {
-    basePath: basePath, // Expose basePath to the entire app
+    basePath: isGithubActions ? `/${repoName}` : '',
   },
 };
 
 export default nextConfig;
-
