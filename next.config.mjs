b18d25fafc470b103dbd26a36b21db5979d9a03a
Chas3Pap3rs <futurefineants@gmail.com>
2024-11-12 13:13:42 -0500

    Images loaded successfully, these changes are to now test the style paths to fix issues in domain. GH pages still working as expected to my knowledge

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
+// next.config.js
+
 const isGithubActions = process.env.GITHUB_ACTIONS || false;
+const isHostinger = process.env.HOSTINGER_DEPLOYMENT || false; // custom environment variable
+
 const repoName = isGithubActions ? process.env.GITHUB_REPOSITORY.split('/')[1] : '';
 
 const nextConfig = {
@@ -7,7 +11,7 @@ const nextConfig = {
     unoptimized: true,
   },
   assetPrefix: isGithubActions ? `/${repoName}/` : '',
-  basePath: isGithubActions ? `/${repoName}` : '',
+  basePath: isGithubActions && !isHostinger ? `/${repoName}` : '', // Set basePath only for GitHub Pages
 };
 
 export default nextConfig;
