cdb764b06b340910dfbd68525c0f69d662868c75
Chas3Pap3rs <futurefineants@gmail.com>
2024-11-17 11:30:28 -0500

    Updating backup

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,16 @@
 const isGithubActions = process.env.GITHUB_ACTIONS || false;
-const repoName = isGithubActions ? process.env.GITHUB_REPOSITORY.split('/')[1] : '';
+const isProduction = process.env.NODE_ENV === "production";
+const isGitHubPages = typeof window !== "undefined" && window.location.hostname === "chas3pap3rs.github.io";
 
 const nextConfig = {
   output: 'export',
   images: {
-    unoptimized: true,
+    unoptimized: true, // Disable image optimization to prevent Next.js from using a server-based image optimization service
   },
-  assetPrefix: isGithubActions ? `/${repoName}/` : '',
-  basePath: isGithubActions ? `/${repoName}` : '',
+  assetPrefix: isGitHubPages ? `/${process.env.GITHUB_REPOSITORY.split('/')[1]}/` : '', // Only apply basePath for GitHub Pages
+  basePath: isGitHubPages ? `/${process.env.GITHUB_REPOSITORY.split('/')[1]}` : '', // Only apply basePath for GitHub Pages
+
+  // For Hostinger, the basePath should not be used
 };
 
 export default nextConfig;
