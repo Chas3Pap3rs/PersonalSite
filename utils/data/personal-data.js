29b7a3963e2c4697048e8170e3e5c289575a7678
Chas3Pap3rs <futurefineants@gmail.com>
2025-01-16 14:07:59 -0500

    Updated profile picture

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -1,6 +1,6 @@
 export const personalData = {
     name: "I'M A PASSIONATE...",
-    profile: '/cedarpoint.jpg',
+    profile: '/profilepic.png',
     designation: "Based in Knoxville, TN",
     description: "I’m passionate about Frontend Development and continuously refine my expertise in JavaScript and React to create intuitive, clean, and efficient code. Whether working on playful web applications or complex simulations, I’m driven by learning and innovation, always staying ahead of development trends.",
     about: "From a young age, I’ve been fascinated by technology and its potential to change the world. With a strong foundation in JavaScript and React, i’m always eager to learn new technologies and tools to improve my skills and create better user experiences. I’m currently seeking opportunities to work with a team of talented developers to build innovative web applications and solve complex problems.",
