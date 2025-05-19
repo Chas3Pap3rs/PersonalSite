8c8549fb51c9768050a14439646c1dd95909ead0
Chas3Pap3rs <chasepoulton@gmail.com>
2025-05-18 21:07:16 -0400

    More changes for deploy fix, used deprecated commands and properties

--- next.config.mjs
+++ next.config.mjs
@@ -7,7 +7,6 @@ const isProd = process.env.NODE_ENV === 'production';
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-  target: 'serverless',
   output: 'export',
   basePath: '',
   assetPrefix: isProd ? '' : undefined,
