2028bad2fd177323c75f2d65a0ebba1aea9c5d73
Chas3Pap3rs <chasepoulton@gmail.com>
2025-05-18 21:04:27 -0400

    Fix for deployment error

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,16 @@
 const isGithubActions = process.env.GITHUB_ACTIONS || false;
 const isProduction = process.env.NODE_ENV === "production";
 const isGitHubPages = typeof window !== "undefined" && window.location.hostname === "chas3pap3rs.github.io";
+const isProd = process.env.NODE_ENV === 'production';
 
+/**
+ * @type {import('next').NextConfig}
+ */
 const nextConfig = {
+  target: 'serverless',
   output: 'export',
+  basePath: '',
+  assetPrefix: isProd ? '' : undefined,
   images: {
     unoptimized: true, // Disable image optimization to prevent Next.js from using a server-based image optimization service
   },
