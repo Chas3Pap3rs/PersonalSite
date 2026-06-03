22ae89c91fb4141cf86b1315fdae72e8ba70d2c0
Chas3Pap3rs <chasepoulton@gmail.com>
2026-06-02 20:38:42 -0400

    Clean up next.config.mjs for VPS deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,22 +1,9 @@
-const isGithubActions = process.env.GITHUB_ACTIONS || false;
-const isProduction = process.env.NODE_ENV === "production";
-const isGitHubPages = typeof window !== "undefined" && window.location.hostname === "chas3pap3rs.github.io";
-const isProd = process.env.NODE_ENV === 'production';
-
-/**
- * @type {import('next').NextConfig}
- */
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '',
-  assetPrefix: isProd ? '' : undefined,
   images: {
-    unoptimized: true, // Disable image optimization to prevent Next.js from using a server-based image optimization service
+    unoptimized: true,
   },
-  assetPrefix: isGitHubPages ? `/${process.env.GITHUB_REPOSITORY.split('/')[1]}/` : '', // Only apply basePath for GitHub Pages
-  basePath: isGitHubPages ? `/${process.env.GITHUB_REPOSITORY.split('/')[1]}` : '', // Only apply basePath for GitHub Pages
-
-  // For Hostinger, the basePath should not be used
 };
 
 export default nextConfig;
